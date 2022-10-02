a5ed4f760bb5fe06154e0cacd5ed49fc02f99046
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 14:39:38 -0400

    Updates

---
+++ precache-manifest.f9d60ca81c6737729bf2a2557d240ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a9428cb51093539509773cae911250",
+    "url": "/ColorGenerator/index.html"
+  },
+  {
+    "revision": "c8dd07fe526b170c8c01",
+    "url": "/ColorGenerator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/ColorGenerator/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/ColorGenerator/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8dd07fe526b170c8c01",
+    "url": "/ColorGenerator/static/js/main.5cf63f8b.chunk.js"
+  },
+  {
+    "revision": "5cd393ef5cfd7c35de60",
+    "url": "/ColorGenerator/static/js/runtime-main.6a8392e4.js"
+  }
+]);
